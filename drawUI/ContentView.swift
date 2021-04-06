755d2f8062634c9e89074e06076630371bb1765b
Andeline <andelineya@gmail.com>
2021-04-06 19:34:40 +0800

    Initial Commit

---
+++ drawUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  drawUI
+//
+//  Created by 蔡佳穎 on 2021/4/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
